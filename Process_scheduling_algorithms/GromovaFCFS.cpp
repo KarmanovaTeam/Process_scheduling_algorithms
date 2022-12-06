af27bbb332378d2f7dcf61c4078ceaba6fd6d359
KarmanovaAnn <113035284+KarmanovaAnn@users.noreply.github.com>
2022-12-06 15:37:35 +0300

    Update GromovaFCFS.cpp

--- Process_scheduling_algorithms/GromovaFCFS.cpp
+++ Process_scheduling_algorithms/GromovaFCFS.cpp
@@ -66,3 +66,121 @@ int main()
 
     return 0;
 }
+
+
+//Другой вариант кода
+/*#include <vector>
+#include<iostream>
+
+using namespace std;
+
+// Function to find the waiting time for all 
+// processes
+
+void findWaitingTime(vector <int>& processes, const int& n, vector <int>& bt, vector <int>& wt)
+{
+
+    // waiting time for first process is 0
+
+    wt[0] = 0;
+
+
+    // calculating waiting time
+
+    for (int i = 1; i < n; i++)
+
+        wt[i] = bt[i - 1] + wt[i - 1];
+}
+
+// Function to calculate turn around time
+
+void findTurnAroundTime(vector <int>& processes, const int& n, vector <int>& bt, vector <int>& wt, vector <int>& tat)
+{
+
+    // calculating turnaround time by adding
+
+    // bt[i] + wt[i]
+
+    for (int i = 0; i < n; i++)
+
+        tat[i] = bt[i] + wt[i];
+}
+
+//Function to calculate average time
+
+void findavgTime(vector <int>& processes, const int& n, vector <int>& bt)
+{
+    vector <int> wt, tat;
+    wt.resize(n);
+    tat.resize(n);
+
+    int total_wt = 0, total_tat = 0;
+
+
+    //Function to find waiting time of all processes
+
+    findWaitingTime(processes, n, bt, wt);
+
+
+    //Function to find turn around time for all processes
+
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+
+    //Display processes along with all details
+
+    //cout << "Processes  " << " Burst time  "
+
+        //<< " Waiting time  " << " Turn around time\n";
+
+
+    // Calculate total waiting time and total turn 
+
+    // around time
+
+    for (int i = 0; i < n; i++)
+
+    {
+
+        total_wt = total_wt + wt[i];
+
+        total_tat = total_tat + tat[i];
+
+        /*cout << "   " << i + 1 << "\t\t" << bt[i] << "\t    "
+
+            << wt[i] << "\t\t  " << tat[i] << endl;*/
+        cout << processes[i] << " ";
+
+    }
+    
+
+    cout <<endl<< "Average waiting time = "
+
+        << (float)total_wt / (float)n;
+
+    cout << "\nAverage turn around time = "
+
+        << (float)total_tat / (float)n;
+}
+
+// Driver code
+
+int main()
+{
+
+    //process id's
+    vector <int> processes = { 1, 2, 3, 4, 5 };
+    //int processes[] = { 1, 2, 3 };
+
+    //int n = sizeof processes / sizeof processes[0];
+    const int n = processes.size();
+
+    //Burst time of all processes
+
+    vector <int> burst_time = { 10, 5, 8, 6, 7 };
+
+
+    findavgTime(processes, n, burst_time);
+
+    return 0;
+}*/
