ba371a9ed3d7f9e12f637bd8223f75995424e9ce
Pisce5s <113035083+Pisce5s@users.noreply.github.com>
2022-12-10 13:07:47 +0300

    Update Process_scheduling_algorithms.cpp

--- Process_scheduling_algorithms/Process_scheduling_algorithms.cpp
+++ Process_scheduling_algorithms/Process_scheduling_algorithms.cpp
@@ -1,5 +1,6 @@
 #include<iostream> 
 #include<vector> 
+#include"SJN.cpp" 
 using namespace std;
 
 
@@ -22,6 +23,7 @@ int main() {
     //ПОЯСНЕНИЕ : {3, 18, HIGH} 3-номер процесса, 18 - burst_time, HIGH - приоритетность, если приоритетность не стоит - значит, она по умолчанию NORMAL
     const int quantum = 2; //квант времения
     vector<Process> processes = { {1, 10},{2, 4}, {3, 18, HIGH}, {4, 99, LOW}, {5, 2}, {6, 55}, {7, 1000},{8, 23, LOW},{9, 10},{10, 100000, HIGH} };
+    SJN_avg_time(processes);
     //Тест 2
     /*const int quantum = 10; //квант времения
     vector<Process> processes = { {1, 100000},{2, 1000}, {3, 5000, HIGH}, {4, 2000, HIGH}, {5, 100000, HIGH}};*/
