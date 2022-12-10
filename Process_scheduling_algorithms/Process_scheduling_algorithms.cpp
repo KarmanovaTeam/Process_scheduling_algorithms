dd4d80af7e8a0ed5ce83fcd0895c113564d002ea
Maxim050903 <113034807+Maxim050903@users.noreply.github.com>
2022-12-10 14:25:32 +0300

    Update Process_scheduling_algorithms.cpp

--- Process_scheduling_algorithms/Process_scheduling_algorithms.cpp
+++ Process_scheduling_algorithms/Process_scheduling_algorithms.cpp
@@ -1,6 +1,7 @@
 #include<iostream> 
 #include<vector> 
-#include"SJN.cpp" 
+#include"SJN.cpp"
+#include"syuzev_dz.cpp"
 using namespace std;
 
 
@@ -24,6 +25,7 @@ int main() {
     const int quantum = 2; //квант времения
     vector<Process> processes = { {1, 10},{2, 4}, {3, 18, HIGH}, {4, 99, LOW}, {5, 2}, {6, 55}, {7, 1000},{8, 23, LOW},{9, 10},{10, 100000, HIGH} };
     SJN_avg_time(processes);
+    findavgTime(processes, quantum);
     //Тест 2
     /*const int quantum = 10; //квант времения
     vector<Process> processes = { {1, 100000},{2, 1000}, {3, 5000, HIGH}, {4, 2000, HIGH}, {5, 100000, HIGH}};*/
