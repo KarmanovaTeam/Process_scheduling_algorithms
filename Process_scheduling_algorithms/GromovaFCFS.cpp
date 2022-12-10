ee97976d528fc84a153e78fbce31a2420a68640d
Anastasia228977 <113035261+Anastasia228977@users.noreply.github.com>
2022-12-10 17:18:11 +0300

    Update GromovaFCFS.cpp

--- Process_scheduling_algorithms/GromovaFCFS.cpp
+++ Process_scheduling_algorithms/GromovaFCFS.cpp
@@ -2,59 +2,71 @@
 #include<iostream>
 using namespace std;
 
-void sortProcess(vector <int>& n, vector <int>& at)
+template <typename T>
+void sortProcess((vector <T>& processes, const int& n)
 {
-    for (int i = 0;i < 5;i++)
+    vector <int> at;
+    vector <int> num;
+    rem_bt.resize(n);
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0;j < 4;j++)
+        at[i] = processes[i].arriving_time;
+        num[i] = processes[i].Pid;
+    }
+
+    for (int i = 0;i < n;i++)
+    {
+        for (int j = 0;j < n-1;j++)
         {
          
             if (at[i] < at[j])
             {
                 int Na,T;
-                Na = n[i];
-                n[i] = n[j];
-                n[j] = Na;
+                Na = num[i];
+                num[i] = num[j];
+                num[j] = Na;
 
                 T = at[i];
                 at[i] = at[j];
                 at[j] = T;
             
             }
-         
         }
     }
     for (int i = 0;i < 5;i++)
     {
-        cout << n[i] << "\t";
+        cout << num[i] << "\t";
     }
 }
 
 
 
-void findWaitingTime(vector <int>& processes, const int& n, vector <int>& bt, vector <int>& wt)
+template <typename T>
+void findWaitingTime(vector <T>& processes, const int& n, vector <int>& wt)
 {
     // waiting time for first process is 0
     wt[0] = 0;
     for (int i = 1; i < n; i++)
-        wt[i] = bt[i - 1] + wt[i - 1];
+        wt[i] = processes[i - 1].burst_time + wt[i - 1];
 }
-
-void findTurnAroundTime(vector <int>& processes, const int& n, vector <int>& bt, vector <int>& wt, vector <int>& tat)
+template <typename T>
+void findTurnAroundTime(vector <T>& processes, const int& n, vector <int>& wt, vector <int>& tat)
 {
    
     for (int i = 0; i < n; i++)
-        tat[i] = bt[i] + wt[i];
+        tat[i] = processes[i].burst_time + wt[i];
 }
 //Function to calculate average time
-void findavgTime(vector <int>& processes, const int& n, vector <int>& bt, vector <int>& arrival_time)
+template <typename T>
+void findavgTime(vector <T>& processes)
 {
+    const int n = processes.size();
     vector <int> wt, tat;
     wt.resize(n);
     tat.resize(n);
     int total_wt = 0, total_tat = 0;
-    findWaitingTime(processes, n, bt, wt);
-    findTurnAroundTime(processes, n, bt, wt, tat);
+    findWaitingTime(processes, n, wt);
+    findTurnAroundTime(processes, n, wt, tat);
     //Display processes along with all details
     //cout << "Processes  " << " Burst time  "
         //<< " Waiting time  " << " Turn around time\n";
@@ -71,7 +83,7 @@ void findavgTime(vector <int>& processes, const int& n, vector <int>& bt, vector
     }
 
     cout << "First Come First Serve(FCFS)" << endl;
-    sortProcess(processes, arrival_time);
+    sortProcess(processes, n);
 
     cout << endl << "Average waiting time = "
 
