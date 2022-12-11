936fd9891da67678aab1729bda0a97790e84ed40
Natalya775 <113034218+Natalya775@users.noreply.github.com>
2022-12-11 18:24:30 +0300

    Update MultilevelFeedbackQueue_Galyaeva1.cpp

--- Process_scheduling_algorithms/MultilevelFeedbackQueue_Galyaeva1.cpp
+++ Process_scheduling_algorithms/MultilevelFeedbackQueue_Galyaeva1.cpp
@@ -12,31 +12,68 @@ vector <process> Q1;
 vector <process> Q2;
 vector <process> Q3;
 
-int n;
-void sortByArrival()
+//int n;
+//void sortByArrival()
+//{
+//	struct process temp;
+//	int i, j;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = i + 1; j < n; j++)
+//		{
+//			if (Q1[i].AT > Q1[j].AT)
+//			{
+//				temp = Q1[i];
+//				Q1[i] = Q1[j];
+//				Q1[j] = temp;
+//			}
+//		}
+//	}
+//}
+
+template <typename T>
+void sortByArrival(vector <T>& processes, const int& n)
 {
-	struct process temp;
-	int i, j;
-	for (i = 0; i < n; i++)
+	//vector <int> at;
+	//vector <int> num;
+	Q1.resize(n);
+	//num.resize(n);
+	for (int i = 0; i < n; i++)
 	{
-		for (j = i + 1; j < n; j++)
+		Q1[i].AT = processes[i].arriving_time;
+		Q1[i].name = processes[i].Pid;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - 1; j++)
 		{
-			if (Q1[i].AT > Q1[j].AT)
+
+			if (Q1[i].AT < Q1[j].AT)
 			{
-				temp = Q1[i];
-				Q1[i] = Q1[j];
-				Q1[j] = temp;
+				int Na, A;
+				Na = Q1[i].name;
+				Q1[i].name = Q1[j].name;
+				Q1[j].name = Na;
+
+				A = Q1[i].AT;
+				Q1[i].AT = Q1[j].AT;
+				Q1[j].AT = A;
+
 			}
 		}
 	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cout << num[i] << "\t";
+//	}
 }
 
 template <typename T>
-void Function(vector <T>& processes)
+void MLFQ(vector <T>& processes)
 {
 	int i, j, k = 0, r = 0, time = 0, tq1 = 5, tq2 = 8, flag = 0;
 	int c;
-	n = 5;
+	const int n = processes.size();
 	Q1.resize(n);
 	Q2.resize(n);
 	Q3.resize(n);
@@ -47,7 +84,7 @@ void Function(vector <T>& processes)
 		Q1[i].AT = processes[i].arriving_time;
 		Q1[i].RT = Q1[i].BT;/*save burst time in remaining time for each process*/
 	}
-	sortByArrival();
+	sortByArrival(processes, n);
 	time = Q1[0].AT;
 	printf("Process in first queue following RR with qt=5");
 	printf("\nProcess\t\tTAT\t\t");
@@ -77,8 +114,8 @@ void Function(vector <T>& processes)
 	}
 	if (flag == 1)
 	{
-		 printf("\nProcess in second queue following RR with qt=8");
-		 printf("\nProcess\t\tTAT\t\t"); 
+		printf("\nProcess in second queue following RR with qt=8");
+		printf("\nProcess\t\tTAT\t\t");
 	}for (i = 0; i < k; i++)
 	{
 		if (Q2[i].RT <= tq2)
@@ -89,7 +126,6 @@ void Function(vector <T>& processes)
 			Q2[i].TAT = time - Q2[i].AT;/*amount of time to execute the process*/
 			printf("\n%d\t\t%d\t\t", Q2[i].name, Q2[i].TAT);
 			//printf("\n%d\t\t%d\t\t%d\t\t%d", Q2[i].name, Q2[i].BT, Q2[i].WT, Q2[i].TAT);
-
 		}
 		else/*process moves to queue 3 with FCFS*/
 		{
@@ -106,7 +142,7 @@ void Function(vector <T>& processes)
 	{
 		if (flag == 2)
 			printf("\nProcess in third queue following FCFS ");
-			printf("\nProcess\t\tTAT\t\t");
+		printf("\nProcess\t\tTAT\t\t");
 	}
 	for (i = 0; i < r; i++)
 	{
