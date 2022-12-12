b622ee5f3dff5a46003c60c54ca73acd151e6ac1
Maxim050903 <cuzevm@mail.ru>
2022-12-12 18:59:34 +0300

    Update syuzev_dz.cpp

--- Process_scheduling_algorithms/syuzev_dz.cpp
+++ Process_scheduling_algorithms/syuzev_dz.cpp
@@ -59,6 +59,8 @@ void findavgTime(vector <Process>& processes, const int& quantum)
         total_tat = total_tat + tat[i];
         cout  << processes[i].Pid << " ";
     }
+    cout<<fixed;
+    cout.precision(2);
     cout <<endl<< "Average waiting time = "
         << (float)total_wt / (float)n;
     cout << "\nAverage turn around time = "
