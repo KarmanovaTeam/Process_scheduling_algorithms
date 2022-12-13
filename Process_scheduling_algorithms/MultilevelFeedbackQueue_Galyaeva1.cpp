913b451dea7a303808e6cec33def79120d7f94fd
Natalya775 <113034218+Natalya775@users.noreply.github.com>
2022-12-13 03:23:38 +0300

    Update MultilevelFeedbackQueue_Galyaeva1.cpp

--- Process_scheduling_algorithms/MultilevelFeedbackQueue_Galyaeva1.cpp
+++ Process_scheduling_algorithms/MultilevelFeedbackQueue_Galyaeva1.cpp
@@ -13,25 +13,6 @@ vector <process> Q1;
 vector <process> Q2;
 vector <process> Q3;
 
-//int n;
-//void sortByArrival()
-//{
-// struct process temp;
-// int i, j;
-// for (i = 0; i < n; i++)
-// {
-// for (j = i + 1; j < n; j++)
-// {
-// if (Q1[i].AT > Q1[j].AT)
-// {
-// temp = Q1[i];
-// Q1[i] = Q1[j];
-// Q1[j] = temp;
-// }
-// }
-// }
-//}
-
 template <typename T>
 void sortByArrival(vector <T>& processes, const int& n)
 {
@@ -56,14 +37,13 @@ Q1[j].name = Na;
 A = Q1[i].AT;
 Q1[i].AT = Q1[j].AT;
 Q1[j].AT = A;
-
 }
 }
 }
-// for (int i = 0; i < 5; i++)
-// {
-// cout « num[i] « "\t";
-// }
+ for (int i = 0; i < n; i++)
+ {
+	 cout << Q1[i].name << "\t";
+ }
 }
 
 template <typename T>
@@ -83,17 +63,14 @@ Q1[i].BT = processes[i].burst_time;;
 Q1[i].AT = processes[i].arriving_time;
 Q1[i].RT = Q1[i].BT;/*save burst time in remaining time for each process*/
 }
+cout << "Multilevel Feedback Queue: " << "\n";
 sortByArrival(processes, n);
 time = Q1[0].AT;
-cout « "Multilevel Feedback Queue: " « "\n";
 //printf("Process in first queue following RR with qt=5");
-//printf("\nProcess\t\tTAT\t\t");
 for (i = 0; i < n; i++)
 {
-
 if (Q1[i].RT <= tq1)
 {
-
 time += Q1[i].RT;/*from arrival time of first process to completion of this process*/
 Q1[i].RT = 0;
 Q1[i].WT = time - Q1[i].AT - Q1[i].BT;/*amount of time process has been waiting in the first queue*/
@@ -111,16 +88,11 @@ Q2[k].RT = Q2[k].BT;
 Q2[k].name = Q1[i].name;
 k = k + 1;
 flag = 1;
-//printf("\n%d\t\t%d\t\t", Q1[i].name, Q1[i].TAT);
-cout « Q1[i].name « "\t";
-//cout « Q1[i].TAT « " " « "\n";
 }
 }
 if (flag == 1)
 {
 //printf("\nProcess in second queue following RR with qt=8");
-//printf("\nProcess\t\tTAT\t\t");
-//}
 for (i = 0; i < k; i++)
 {
 if (Q2[i].RT <= tq2)
@@ -131,9 +103,6 @@ Q2[i].WT = time - tq1 - Q2[i].BT;/*amount of time process has been waiting in th
 Q2[i].TAT = time - Q2[i].AT;/*amount of time to execute the process*/
 total_wt = total_wt + Q2[i].WT;
 total_tat = total_tat + Q2[i].TAT;
-cout « Q2[i].name « "\t";
-//printf("\n%d\t\t%d\t\t", Q2[i].name, Q2[i].TAT);
-//printf("\n%d\t\t%d\t\t%d\t\t%d", Q2[i].name, Q2[i].BT, Q2[i].WT, Q2[i].TAT);
 }
 else/*process moves to queue 3 with FCFS*/
 {
@@ -151,8 +120,6 @@ flag = 2;
 {
 if (flag == 2)
 //printf("\nProcess in third queue following FCFS ");
-//printf("\nProcess\t\tTAT\t\t");
-//}
 for (i = 0; i < r; i++)
 {
 if (i == 0)
@@ -166,8 +133,6 @@ Q3[i].TAT = Q3[i].CT;
 Q3[i].WT = Q3[i].TAT - Q3[i].BT;
 total_wt = total_wt + Q3[i].WT;
 total_tat = total_tat + Q3[i].TAT;
-cout « Q3[i].name « "\t";
-//printf("\n%d\t\t%d\t\t", Q3[i].name, Q3[i].TAT);
 }
 }
   double a,b;
@@ -176,13 +141,13 @@ a = round(a * 100.0) /
 100.0;
 b = (double)total_tat / (double)n;
 b = round(b * 100.0) / 100.0;
-cout « fixed;
+cout << fixed;
 cout.precision(2);
-cout « endl « "Average waiting time = "
-« (a);
+cout << endl << "Average waiting time = "
+<< (a);
 
-cout « "\nAverage turn around time = "
+cout << "\nAverage turn around time = "
 
-« (b);
-cout « endl;
+<< (b);
+cout << endl;
 }
